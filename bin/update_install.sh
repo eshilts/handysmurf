c5086515c6db869f007c6907be914c089363973e
erik 'quantitative warlock' shilts <erik.shilts@opower.com>
2011-12-07 11:51:17 -0500

    logging

--- bin/update_install.sh
+++ bin/update_install.sh
@@ -11,7 +11,9 @@ fi
 echo `pwd`
 git pull git@github.opower.com:eshilts/handysmurf.git master
 cd ..
+echo `pwd`
 handysmurf/bin/roxygenize.R
+echo `pwd`
 echo 'Building handysmurf.'
 R CMD BUILD handysmurf
 cd ~
