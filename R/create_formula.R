8aff17c7f1bede29b15e31b063984c1183272019
erik 'quantitative warlock' shilts <erik.shilts@opower.com>
2012-02-04 11:03:11 -0500

    function to quickly create a formula object

---
+++ R/create_formula.R
@@ -0,0 +1,11 @@
+#' Creates Formula Object From Strings
+#' Simplifies creation of a formula object given strings of variables names.
+#'
+#' @param lhs name of left hand side (dependent) variable; must be a singleton
+#' @param rhs name(s) of right hand side (independent) variables
+#' @author Erik Shilts
+#' @export
+create_formula <- function(lhs, rhs) {
+  stopifnot(unique(lhs) == 1)
+  as.formula(sprintf('%s ~ %s', unique(lhs), paste(unique(rhs), collapse = '+')))
+}
