f4bcfc371706879e078a2c0149cba70eeaf536d7
erik 'quantitative warlock' shilts <erik.shilts@opower.com>
2012-03-07 11:27:10 -0500

    function to count the number of missing observations in each column of a data.frame

---
+++ R/count_NA.R
@@ -0,0 +1,11 @@
+#' Count NA Observations in a Dataframe
+#' Counts the number of missing observations in each columns of a data.frame.
+#'
+#' @param dat input data.frame
+#' @author Erik Shilts
+#' @export
+#' @examples
+#' df <- data.frame(a = 1:10, b = rep(NA, 10), c=rep(c(1, NA), 5))
+#' count_NA(df)
+count_NA <- function(dat)
+  sapply(dat, function(x) sum(is.na(x)))
